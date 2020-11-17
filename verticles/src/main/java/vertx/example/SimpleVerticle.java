49f3a35905587482fa56b4ab693c8840a5310ab2
alexey.zhukov <alexey.zhukov@corp.mail.ru>
2020-11-17 17:19:35 +0300

    add vertx example files

--- verticles/src/main/java/vertx/example/SimpleVerticle.java
+++ verticles/src/main/java/vertx/example/SimpleVerticle.java
@@ -1,12 +1,19 @@
 package vertx.example;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
 
-@SuppressWarnings({"MagicNumber", "UseOfSystemOutOrSystemErr"})
+@SuppressWarnings({"MagicNumber", "UseOfSystemOutOrSystemErr", "NotNullNullableValidation"})
 public final class SimpleVerticle extends AbstractVerticle {
 
   @Override
-  public void start() {
-    System.out.println("Start Simple");
+  public void start(Promise<Void> startPromise) {
+    vertx.executeBlocking(promise -> {
+      System.out.println("Call some blocking API");
+      promise.complete();
+    }, res -> {
+      System.out.println("Start Simple");
+      startPromise.complete();
+    });
   }
 }
